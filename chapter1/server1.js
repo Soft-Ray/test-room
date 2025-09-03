bef832cf5ecd506716de140d121967763aad6149
Soft-Ray <mk11166789@gmail.com>
2025-09-03 13:34:59 +0900

    Update server1.js

--- chapter1/server1.js
+++ chapter1/server1.js
@@ -334,7 +334,7 @@ function createContext() {
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'server1.html'));
+  res.sendFile(path.join(__dirname, '..', 'server.html'));
 });
 
 app.post('/chat', async (req, res) => {
@@ -590,5 +590,6 @@ const start = () => {
 if (import.meta.url === `file://${process.argv[1]}`) {
   start();
 }
+
 
 
