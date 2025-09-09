0525eed656e74ef0a690aa4402a7a2cb1702d5ae
Soft-Ray <mk11166789@gmail.com>
2025-09-09 18:41:30 +0900

    Update server2.js

--- chapter2/server2.js
+++ chapter2/server2.js
@@ -41,7 +41,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './'), { index: false }));
+app.use(express.static(path.join(__dirname, './'), { index: 'server.html' }));
 
 // OpenAI 초기화
 const openai = new OpenAI({
